5b3b4bb476ebf4995b7eb24b935c0aa5fd0a574c
Tolik <anatolijparygin988@gmail.com>
2022-12-08 20:30:58 +0300

    сумма элементов на нечетных элементов массива

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+const int SIZE = 5;
+const int LEFT_RANGE = 1;
+const int RIGHT_RANGE = 100;
+int[] array = new int[SIZE];
+int[] FillArrayWithRandomNambers(int size, int leftRange, int rightRenge)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRenge + 1);
+    }
+    return arr;
+}
+
+array = FillArrayWithRandomNambers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine('[' + string.Join(",", array) + ']');
+int sum = 0;
+for (int index = 0; index < array.Length; index++)
+{
+    if (index % 2 != 0)
+    {
+        sum = sum + array[index];
+    }
+}
+Console.Write("  Сумма нетных элементов массива равно: " + sum);

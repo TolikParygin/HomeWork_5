f7c77d7182e62be15bebe67c77103fabcdc2c863
Tolik <anatolijparygin988@gmail.com>
2022-12-08 20:28:01 +0300

    количество четных элементов в массиве

---
+++ Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Clear();
+const int SIZE = 10;
+const int LEFT_RANGE = 1;
+const int RIGHT_RANGE = 100;
+int[] array = new int[SIZE];
+int[] FillArrayWithRandomNambers(int size, int leftRange, int rightRenge)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRenge + 1);
+    }
+    return arr;
+}
+
+array = FillArrayWithRandomNambers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine('[' + string.Join(",", array) + ']');
+int sum = 0;
+for (int index = 0; index < array.Length; index++)
+{
+    
+    if (array[index] % 2 == 0)
+    {
+        sum++;
+    }
+}
+Console.Write("Количество четных элементов массива равно: " + sum);
